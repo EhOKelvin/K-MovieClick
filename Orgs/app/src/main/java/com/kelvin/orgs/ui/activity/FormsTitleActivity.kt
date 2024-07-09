57f23b4baf96e821a16c226ebb38ce5b65fadebe
EhOKelvin <kbsilva064@gmail.com>
2024-07-09 10:10:35 -0300

    Adicionando alteracoes

---
+++ Orgs/app/src/main/java/com/kelvin/orgs/ui/activity/FormsTitleActivity.kt
@@ -0,0 +1,11 @@
+package com.kelvin.orgs
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FormsTitleActivity : AppCompatActivity(R.layout.activity_forms_title) {
+
+    }
